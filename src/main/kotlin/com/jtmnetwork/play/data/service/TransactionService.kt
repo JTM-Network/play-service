180cf959daf7c83ed214fed4c4d56b7a0aa0ea4e
Joe Matthews <joetymatthews@gmail.com>
2022-08-27 19:16:46 +0100

    new: add transaction service logic

--- src/main/kotlin/com/jtmnetwork/play/data/service/TransactionService.kt
+++ src/main/kotlin/com/jtmnetwork/play/data/service/TransactionService.kt
@@ -1,22 +1,37 @@
 package com.jtmnetwork.play.data.service
 
 import com.jtmnetwork.play.core.domain.entity.Transaction
+import com.jtmnetwork.play.core.domain.exception.transaction.TransactionNotFound
 import com.jtmnetwork.play.core.usecase.repository.TransactionRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.util.UUID
 
 @Service
 class TransactionService @Autowired constructor(private val transactionRepository: TransactionRepository) {
 
-    fun insertTransaction(transaction: Transaction): Mono<Transaction> = Mono.empty()
+    fun insertTransaction(transaction: Transaction): Mono<Transaction> {
+        return transactionRepository.save(transaction)
+    }
 
-    fun updateTransaction(transaction: Transaction): Mono<Transaction> = Mono.empty()
+    fun updateTransaction(transaction: Transaction): Mono<Transaction> {
+        return transactionRepository.findById(transaction.id)
+            .switchIfEmpty(Mono.defer { Mono.error(TransactionNotFound()) })
+            .flatMap { transactionRepository.save(transaction) }
+    }
 
-    fun getTransaction(): Mono<Transaction> = Mono.empty()
+    fun getTransaction(id: UUID): Mono<Transaction> {
+        return transactionRepository.findById(id)
+            .switchIfEmpty(Mono.defer { Mono.error(TransactionNotFound()) })
+    }
 
-    fun getTransactions(): Flux<Transaction> = Flux.empty()
+    fun getTransactions(): Flux<Transaction> = transactionRepository.findAll()
 
-    fun removeTransactions(): Mono<Transaction> = Mono.empty()
+    fun removeTransactions(id: UUID): Mono<Transaction> {
+        return transactionRepository.findById(id)
+            .switchIfEmpty(Mono.defer { Mono.error(TransactionNotFound()) })
+            .flatMap { transactionRepository.delete(it).thenReturn(it) }
+    }
 }
