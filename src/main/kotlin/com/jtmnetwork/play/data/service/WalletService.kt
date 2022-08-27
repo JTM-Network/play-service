b60ca658af3da93c1eba6789ead0e490dc96ddcf
Joe Matthews <joetymatthews@gmail.com>
2022-08-27 19:14:55 +0100

    new: add wallet service logic

--- src/main/kotlin/com/jtmnetwork/play/data/service/WalletService.kt
+++ src/main/kotlin/com/jtmnetwork/play/data/service/WalletService.kt
@@ -1,6 +1,8 @@
 package com.jtmnetwork.play.data.service
 
 import com.jtmnetwork.play.core.domain.entity.Wallet
+import com.jtmnetwork.play.core.domain.exception.wallet.WalletFound
+import com.jtmnetwork.play.core.domain.exception.wallet.WalletNotFound
 import com.jtmnetwork.play.core.usecase.repository.WalletRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -10,13 +12,28 @@ import reactor.core.publisher.Mono
 @Service
 class WalletService @Autowired constructor(private val walletRepository: WalletRepository) {
 
-    fun insertWallet(wallet: Wallet): Mono<Wallet> = Mono.empty()
+    fun insertWallet(wallet: Wallet): Mono<Wallet> {
+        return walletRepository.findById(wallet.id)
+            .flatMap<Wallet> { Mono.error(WalletFound()) }
+            .switchIfEmpty(Mono.defer { walletRepository.save(wallet) })
+    }
 
-    fun updateWallet(wallet: Wallet): Mono<Wallet> = Mono.empty()
+    fun updateWallet(wallet: Wallet): Mono<Wallet> {
+        return walletRepository.findById(wallet.id)
+            .switchIfEmpty(Mono.defer { Mono.error(WalletNotFound()) })
+            .flatMap { walletRepository.save(wallet) }
+    }
 
-    fun getWallet(): Mono<Wallet> = Mono.empty()
+    fun getWallet(id: String): Mono<Wallet> {
+        return walletRepository.findById(id)
+            .switchIfEmpty(Mono.defer { Mono.error(WalletNotFound()) })
+    }
 
-    fun getWallets(): Flux<Wallet> = Flux.empty()
+    fun getWallets(): Flux<Wallet> = walletRepository.findAll()
 
-    fun removeWallet(): Mono<Wallet> = Mono.empty()
+    fun removeWallet(id: String): Mono<Wallet> {
+        return walletRepository.findById(id)
+            .switchIfEmpty(Mono.defer { Mono.error(WalletNotFound()) })
+            .flatMap { walletRepository.delete(it).thenReturn(it) }
+    }
 }
